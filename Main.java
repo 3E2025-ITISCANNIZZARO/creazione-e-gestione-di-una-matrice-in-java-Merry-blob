66e627c4610fed2bb1a4a717889a452a8c384d0e
Merry <pogliani.sebastiano@itiscannizzaro.edu.it>
2025-01-15 12:33:59 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,34 @@
-// Esercizio: Lavorare con le Matrici in Java
-//
-// 1. Creare una matrice 3x3 di numeri interi.
-// 2. Popolare la matrice con valori casuali compresi tra 1 e 100.
-// 3. Implementare un metodo che calcoli la somma di tutti gli elementi della matrice.
-// 4. Implementare un metodo che trovi il valore massimo nella matrice.
-// 5. Stampare la matrice, la somma degli elementi e il valore massimo trovato.
-//
-// Suggerimento:
-// - Utilizza la classe Random per generare numeri casuali.
-// - Per stampare la matrice, usa un ciclo annidato.
+import java.util.Arrays; 
 
 public class Main {
     public static void main(String[] args) {
-        // Creazione e popolamento della matrice
-        int[][] matrix = new int[3][3];
-        Random random = new Random();
-        // COMPLETARE IL CODICE
+        int[][] matrix = new int[3][3]; 
+        for(int i = 0; i < matrix[0].length; i++){
+            for(int o = 0; o < matrix[i].length; o++){
+                matrix[i][o] = (int)(Math.random()*100+1);
+            }
+        }
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.toString(matrix[1]));
+        System.out.println(Arrays.toString(matrix[2])); 
 
-      
 
-        // Stampa della matrice
-        System.out.println("Matrice:");
-        // COMPLETARE IL CODICE
-
-
-        // Calcolo della somma degli elementi
-        int sum = calculateSum(matrix);
+        int sum = 0;
+        for(int i = 0; i < matrix[0].length; i++){
+            for(int o = 0; o < matrix[i].length; i++){
+                sum += matrix[i][o];
+            }
+        }
         System.out.println("Somma degli elementi: " + sum);
 
-        // Ricerca del valore massimo
-        int max = findMax(matrix);
+        int max = -1;
+        for(int i = 0; i < matrix[0].length; i++){
+            for(int o = 0; o < matrix[i].length; i++){
+                if(matrix[i][o] > max){
+                    max = matrix[i][o];
+                }
+            }
+        }
         System.out.println("Valore massimo: " + max);
-    }
-
-    public static int calculateSum(int[][] matrix) {
-        int sum = 0;
-   
-// COMPLETARE IL CODICE
-      
-        return sum;
-    }
-
-    public static int findMax(int[][] matrix) {
-        int max = matrix[0][0];
-
-// COMPLETARE IL CODICE
-      
-        return max;
     }
 }
